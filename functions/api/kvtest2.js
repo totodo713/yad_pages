57fbce6cfbf8d46e5d0f551fe1d55fcf7f0f8ec3
yadoya <s.yamagami.666@gmail.com>
2023-12-09 16:08:54 +0900

    temp:fix kvtest

--- functions/api/kvtest2.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-    const task = await context.env.TODO_LIST.get("test");
-    return new Response(task);
-  }export async function onRequest(context) {
-  const task = await context.env.TODO_LIST.get("test");
-  return new Response(task);
-}
